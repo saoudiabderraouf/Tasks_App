8fcb8829a92f4b2fc1bd515e846efaf91446fb81
Abderraouf Saoudi <ha_saoudi@esi.dz>
2021-04-03 18:02:42 +0100

    bugs fixed

--- app/src/main/java/com/example/tasks_app/TasksFragment.java
+++ app/src/main/java/com/example/tasks_app/TasksFragment.java
@@ -35,6 +35,24 @@ public class TasksFragment extends Fragment {
         recyclerView =  view.findViewById(R.id.recycler_view_my_tasks);
         recyclerView1 =  view.findViewById(R.id.recycler_view_days);
 
+        ArrayList<Day> days = getDays();
+
+        CustomAdapter1 adapter1 = new CustomAdapter1(days,getContext());
+        RecyclerView.LayoutManager layoutManager1 = new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false);
+        recyclerView1.setAdapter(adapter1);
+        recyclerView1.setLayoutManager(layoutManager1);
+
+        ArrayList<Task> tasks = getTasks();
+
+        CustomAdapter adapter = new CustomAdapter(tasks,getContext());
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(layoutManager);
+
+        return view;
+    }
+
+    public ArrayList<Day> getDays(){
         ArrayList<Day> days = new ArrayList<>();
         Day day1 = new Day("07","Fri");
         days.add(day1);
@@ -51,29 +69,23 @@ public class TasksFragment extends Fragment {
         Day day7 = new Day("13","Thu");
         days.add(day7);
 
-        CustomAdapter1 adapter1 = new CustomAdapter1(days,getContext());
-        RecyclerView.LayoutManager layoutManager1 = new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false);
-        recyclerView1.setAdapter(adapter1);
-        recyclerView1.setLayoutManager(layoutManager1);
+        return days;
+    }
 
+    public ArrayList<Task> getTasks(){
         ArrayList<Task> tasks = new ArrayList<>();
-        Task task1 = new Task("9:00","Send the documents to the client");
+        Task task1 = new Task("9:00","Send the documents to the client",0);
         tasks.add(task1);
-        Task task2 = new Task("11:00","Get the wireframes as soon as possible");
+        Task task2 = new Task("11:00","Get the wireframes as soon as possible",1);
         tasks.add(task2);
-        Task task3 = new Task("14:00","Make a draft design and send it to review");
+        Task task3 = new Task("14:00","Make a draft design and send it to review",1);
         tasks.add(task3);
-        Task task4 = new Task("16:00","Start Development discussion with design team");
+        Task task4 = new Task("16:00","Start Development discussion with design team",1);
         tasks.add(task4);
-        Task task5 = new Task("21:00","Watch Champions league game");
+        Task task5 = new Task("21:00","Watch Champions league game",2);
         tasks.add(task5);
 
-        CustomAdapter adapter = new CustomAdapter(tasks,getContext());
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(layoutManager);
-
-        return view;
+        return tasks;
     }
 
     public static class CustomAdapter extends RecyclerView.Adapter<TasksFragment.CustomAdapter.ViewHolder> {
@@ -84,7 +96,7 @@ public class TasksFragment extends Fragment {
         public static class ViewHolder extends RecyclerView.ViewHolder {
             private final TextView textViewTasksTime;
             private final TextView textViewTasksTitle;
-            private final ImageView imageViewTask;
+            private final ImageView imageViewTask , iv_1 , iv_2 , iv_3 , iv_4;
 
             public ViewHolder(View view) {
                 super(view);
@@ -93,6 +105,10 @@ public class TasksFragment extends Fragment {
                 textViewTasksTime = (TextView) view.findViewById(R.id.tv_Task_Time);
                 textViewTasksTitle = (TextView) view.findViewById(R.id.tv_Task_Title);
                 imageViewTask = (ImageView) view.findViewById(R.id.iv_task);
+                iv_1 = (ImageView) view.findViewById(R.id.iv_1);
+                iv_2 = (ImageView) view.findViewById(R.id.iv_2);
+                iv_3 = (ImageView) view.findViewById(R.id.iv_3);
+                iv_4 = (ImageView) view.findViewById(R.id.iv_4);
             }
 
             public TextView getTasksNumber() {
@@ -105,6 +121,22 @@ public class TasksFragment extends Fragment {
 
             public  ImageView getTasksImage(){
                 return imageViewTask;
+            }
+
+            public ImageView getIv_1() {
+                return iv_1;
+            }
+
+            public ImageView getIv_2() {
+                return iv_2;
+            }
+
+            public ImageView getIv_3() {
+                return iv_3;
+            }
+
+            public ImageView getIv_4() {
+                return iv_4;
             }
         }
 
@@ -129,19 +161,22 @@ public class TasksFragment extends Fragment {
 
             // Get element from your dataset at this position and replace the
             // contents of the view with that element
-
             viewHolder.getTasksNumber().setText(localDataSet.get(position).taskTime);
             viewHolder.getTasksTitle().setText(localDataSet.get(position).taskTitle);
 
-            if (position == 0){
+            if (localDataSet.get(position).done == 0){
                 viewHolder.getTasksImage().setImageDrawable(ContextCompat.getDrawable(context,R.drawable.row_checked_icon));
                 viewHolder.getTasksTitle().setTextColor(ContextCompat.getColor(context,R.color.white));
                 viewHolder.getTasksTitle().setBackground(ContextCompat.getDrawable(context,R.drawable.background_home_tasks_orange));
-            }else if (position == getItemCount()-1){
-                viewHolder.getTasksImage().setImageDrawable(ContextCompat.getDrawable(context,R.drawable.row_checked_icon));
+                viewHolder.getIv_1().setVisibility(View.INVISIBLE);
+                viewHolder.getIv_2().setVisibility(View.INVISIBLE);
+            }else if (localDataSet.get(position).done == 1){
+                viewHolder.getTasksImage().setImageDrawable(ContextCompat.getDrawable(context,R.drawable.row_later_icon));
             }
             else {
-                viewHolder.getTasksImage().setImageDrawable(ContextCompat.getDrawable(context,R.drawable.row_later_icon));
+                viewHolder.getTasksImage().setImageDrawable(ContextCompat.getDrawable(context,R.drawable.ic_row_night_icon));
+                viewHolder.getIv_3().setVisibility(View.INVISIBLE);
+                viewHolder.getIv_4().setVisibility(View.INVISIBLE);
             }
         }
 
@@ -156,12 +191,13 @@ public class TasksFragment extends Fragment {
 
         private String taskTime;
         private String taskTitle;
+        private int done;
 
-        public Task(String taskTime, String taskTitle) {
+        public Task(String taskTime, String taskTitle, int done) {
             this.taskTime = taskTime;
             this.taskTitle = taskTitle;
+            this.done = done;
         }
-
     }
 
     public static class CustomAdapter1 extends RecyclerView.Adapter<CustomAdapter1.ViewHolder> {
